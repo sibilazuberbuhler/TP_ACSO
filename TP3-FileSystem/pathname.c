2fd6ecd8a6caf22bb25c45e2d7574c2df8f25b76
sibilazuberbuhler <141259738+sibilazuberbuhler@users.noreply.github.com>
2025-05-08 19:28:26 -0300

    Update pathname.c

--- TP3-FileSystem/pathname.c
+++ TP3-FileSystem/pathname.c
@@ -11,6 +11,26 @@
  * TODO
  */
 int pathname_lookup(struct unixfilesystem *fs, const char *pathname) {
-    //Implement code here
-	return 0;
+    if (pathname == NULL || *pathname == '\0') {
+        return -1;
+    }
+
+    int current_inumber = 1; 
+
+    char path_copy[strlen(pathname) + 1];
+    strcpy(path_copy, pathname);
+
+    char *token = strtok(path_copy, "/");
+    while (token != NULL) {
+        struct direntv6 entry;
+
+        if (directory_findname(fs, token, current_inumber, &entry) == -1) {
+            return -1;  
+        }
+
+        current_inumber = entry.d_inumber;
+        token = strtok(NULL, "/");
+    }
+
+    return current_inumber; 
 }
