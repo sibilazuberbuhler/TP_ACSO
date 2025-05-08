79eff6559f6b0520c736206a6979a07f25f47482
sibilazuberbuhler <141259738+sibilazuberbuhler@users.noreply.github.com>
2025-05-08 19:09:54 -0300

    Update directory.c

--- TP3-FileSystem/directory.c
+++ TP3-FileSystem/directory.c
@@ -11,6 +11,34 @@
  */
 int directory_findname(struct unixfilesystem *fs, const char *name,
 		int dirinumber, struct direntv6 *dirEnt) {
-  //Implement your code here
-  return 0;
+    struct inode in;
+
+    if (inode_iget(fs, dirinumber, &in) == -1) {
+        return -1;  
+    }
+
+    int filesize = inode_getsize(&in); //tamaño del archivo en bytes 
+    int offset = 0;
+    char block[DISKIMG_SECTOR_SIZE];
+
+    while (offset < filesize) {
+        int bytes = file_getblock(fs, dirinumber, offset / DISKIMG_SECTOR_SIZE, block);
+        if (bytes <= 0) {
+            return -1;  
+        }
+
+        int cantidad = bytes / sizeof(struct direntv6);  // veo cuantas entradas hay en el bloque. bytes / 16 
+        struct direntv6 *entradas = (struct direntv6 *) block; 
+
+        for (int i = 0; i < cantidad; i++) {
+            if (strncmp(entradas[i].d_name, name, DIRNAMELEN) == 0) {
+                *dirEnt = entradas[i];  
+                return 0;
+            }
+        }
+
+        offset += bytes; //voy al bloque que sigue
+    }
+
+    return -1;  
 }
