0229bb27e648c92073cd0fb213adbe48c41fd23d
sibilazuberbuhler <141259738+sibilazuberbuhler@users.noreply.github.com>
2025-05-10 15:33:30 -0300

    Update file.c

--- TP3-FileSystem/file.c
+++ TP3-FileSystem/file.c
@@ -11,32 +11,23 @@
 int file_getblock(struct unixfilesystem *fs, int inumber, int blockNum, void *buf) {
     struct inode in;
 
-    if (inode_iget(fs, inumber, &in) == -1) { 
-        return -1;
-    }
-
-    int sector = inode_indexlookup(fs, &in, blockNum); //Busco en que sector del disco esta guardado blockNum
-    if (sector == -1) {
-        return -1;
-    }
-
-    int result = diskimg_readsector(fs->dfd, sector, buf); //Leo el sector que me devolvio inode_indexlookup
-    if (result == -1) {
-        return -1;
-    }
-
+    if (inode_iget(fs, inumber, &in) == -1) return -1;
+    
+    int sector = inode_indexlookup(fs, &in, blockNum); 
+    if (sector == -1) return -1;
+    
+    int result = diskimg_readsector(fs->dfd, sector, buf); 
+    if (result == -1) return -1;
+    
     int filesize = inode_getsize(&in);
     int start_byte = blockNum * DISKIMG_SECTOR_SIZE; 
 
-    if (filesize <= start_byte) {
-        return -1;
-    }
+    if (filesize <= start_byte) return -1;
 
     int remaining_bytes = filesize - start_byte;
-    if (remaining_bytes < DISKIMG_SECTOR_SIZE) {
-        return remaining_bytes;
-    } else {
-        return DISKIMG_SECTOR_SIZE;
-    }
+    if (remaining_bytes < DISKIMG_SECTOR_SIZE) return remaining_bytes;
+ 
+    return DISKIMG_SECTOR_SIZE;
+
 }
 
